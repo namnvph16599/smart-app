37188e9f151e92cbe3383def9c45b0cde4eb0e86
nvnam042dev <anh02284@gmail.com>
2023-12-08 10:49:30 +0700

    init project install tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center text-[#ccc]">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
