6c0920e894fad9194902369b72e51382a4e86184
nvnam042dev <anh02284@gmail.com>
2023-12-08 11:17:01 +0700

    react query, axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
 import { ConfigProvider } from "antd";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
+
 function App() {
   return (
-    <ConfigProvider>
-      <h1 className="text-3xl font-bold text-center text-[#ccc]">
-        Hello world!
-      </h1>
-    </ConfigProvider>
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider>
+        <h1 className="text-3xl font-bold text-center text-[#ccc]">
+          Hello world!
+        </h1>
+      </ConfigProvider>
+    </QueryClientProvider>
   );
 }
 
