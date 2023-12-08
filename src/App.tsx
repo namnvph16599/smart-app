378d81e4c27e7e7dca3e11d06a2a152101100e06
nvnam042dev <anh02284@gmail.com>
2023-12-08 11:12:38 +0700

    install antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
+import { ConfigProvider } from "antd";
 
 function App() {
   return (
-    <>
+    <ConfigProvider>
       <h1 className="text-3xl font-bold text-center text-[#ccc]">
         Hello world!
       </h1>
-    </>
+    </ConfigProvider>
   );
 }
 
